2d653abbbe113d08ad7caa70d69d818a1d01a884
salkar <sokolov.sergey.a@gmail.com>
2014-06-04 00:39:20 +0400

    fix test controller

--- test/dummy/app/controllers/image_controller.rb
+++ test/dummy/app/controllers/image_controller.rb
@@ -2,13 +2,11 @@ require 'json'
 
 class ImageController < ApplicationController
   def create
-    #uploader = ImageUploader.new
-    #puts uploader.store!(params["upload_img"])
     if params["upload_img"]
       img_url = Image.create(:img => params["upload_img"]).img.url
     else
       img_url = params["upload_url"]
     end
-    render :text => {:data => img_url}.to_json
+    render json: {:data => img_url}
   end
 end
